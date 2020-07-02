478a19ddc67dfec8997367732663e08464d77b46
marlin-kreatif <marlina.kreatif@gmail.com>
2020-07-02 10:42:24 +0700

    mengkonfigurasi nodemon, client-ui, dan template

---
+++ client/Main.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import {
+    render
+} from 'react-dom'
+import HelloWord from "./HelloWord";
+
+
+render(< HelloWord />, document.getElementById('root'))
---
+++ template.js
@@ -0,0 +1,14 @@
+export default () => {
+    return `<!DOCTYPE html>
+                <html lang="en">
+                <head>
+                    <meta charset="UTF-8"/>
+                    <title>Mern Kickstart</title>
+                </head>
+                <body>
+                    <div id='root'></div>
+                    <script type='text/javascript' src='/dist/bundle.js'></script>
+                    
+                </body>
+            </html>`
+}
