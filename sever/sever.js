162c9d933ba247cac5b713a1ed0af55de444dacf
marlin-kreatif <marlina.kreatif@gmail.com>
2020-07-02 10:55:28 +0700

    serving static files from dist folder

--- sever/sever.js
+++ sever/sever.js
@@ -1,5 +1,9 @@
 import express from "express";
 import devBundle from "./devBundle";
+import path from "path";
+
+const CURRENT_WORKING_DIR = process.cwd();
 
 const app = express();
+app.use('/dist', express.static(path.join(CURRENT_WORKING_DIR, 'dist')))
 devBundle.compile(app);
