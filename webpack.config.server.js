e9a2df48b4ea1712d5ecd53ebcb82fefd55ee857
marlin-kreatif <marlina.kreatif@gmail.com>
2020-07-02 10:21:17 +0700

    konfigurasi webpack untuk client, server, dan production

--- webpack.config.server.js
+++ webpack.config.server.js
@@ -1,7 +1,28 @@
 const path = require('path')
 const webpack = require('webpack')
 const CURRENT_WORKING_DIR = process.cwd()
+const nodeExternals = require('webpack-node-externals')
 
-const config = {}
+const config = {
+    name: "sever",
+    entry: [path.join(CURRENT_WORKING_DIR, './server/server.js')],
+    target: "node",
+    output: {
+        path: path.join(CURRENT_WORKING_DIR, '/dist/'),
+        filename: "sever.generated.js",
+        publicPath: "/dist/",
+        libraryTarget: "commontjs2"
+    },
+    externals: [nodeExternals()],
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: ['babel-loader']
+            }
+        ]
+    }
+}
 
 module.exports = config
